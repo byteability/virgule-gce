4721b857112a6666fca68bb89854aaeb1f38ccab
franksheppard <services@franksheppard.com>
2026-05-26 11:42:26 +0100

    fix: resolve ReferenceError when closing the last tab by calling revokeImageCache instead of undefined clearImageBlobCache

--- src/app.js
+++ src/app.js
@@ -1648,7 +1648,7 @@ function clearEditor() {
   editor.value = "";
   insertImageBtn.disabled = true;
   saveBtn.disabled = true;
-  clearImageBlobCache();
+  revokeImageCache();
   renderPreview("");
 }
 
